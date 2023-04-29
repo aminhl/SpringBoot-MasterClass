9e2a1457151a8f41d99e0264069dfc4997fb515a
aminhl <amin.hlel@esprit.tn>
2023-04-29 22:30:05 +0100

    jackson

--- src/main/java/com/nexthope/springbootmasterclass/customer/CustomFakeRepository.java
+++ src/main/java/com/nexthope/springbootmasterclass/customer/CustomFakeRepository.java
@@ -7,8 +7,8 @@ public class CustomFakeRepository implements CustomerRepo{
     @Override
     public List<Customer> getCustomers() {
         return Arrays.asList(
-                new Customer(1L, "Next Hope"),
-                new Customer(2L, "Protocol Blood")
+                new Customer(1L, "Next Hope", "password1"),
+                new Customer(2L, "Protocol Blood", "password2")
         );
     }
 }
